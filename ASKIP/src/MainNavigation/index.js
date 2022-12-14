bfdf5a25d706b29ba75dff2115eb3355f7813204
jamshed-ali-oip <105854101+jamshed-ali-oip@users.noreply.github.com>
2022-12-14 04:48:21 -0700

    second

--- ASKIP/src/MainNavigation/index.js
+++ ASKIP/src/MainNavigation/index.js
@@ -6,13 +6,25 @@ import AuthNav from './AuthNav';
 import SplashScreen from 'react-native-splash-screen';
 import {useSelector} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
+import messaging from '@react-native-firebase/messaging';
 const Stack = createNativeStackNavigator();
 const MainNavigation = () => {
+  
   useEffect(() => {
     SplashScreen.hide();
   });
   const token = useSelector(state => state?.auth?.credential?.token);
   // console.log(token,"navigation")
+
+  useEffect(()=>{
+    checkToken()
+  },[])
+  const checkToken = async () => {
+    const fcmToken = await messaging().getToken();
+    if (fcmToken) {
+       console.log("fcmToken",fcmToken);
+    } 
+   }
   return (
     <NavigationContainer>
       <Stack.Navigator
