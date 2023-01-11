a4cc2cf05511b8896d232d689c142d481eeb0d69
Muhammad Ahsan Muneer <muneerahsan@outlook.com>
2023-01-11 18:24:28 +0500

    .

--- ASKIP/src/config/config.js
+++ ASKIP/src/config/config.js
@@ -2,7 +2,7 @@
 // export const base_URL = "http://192.168.0.136:3000/api/v1"
 // export const base_URL_IMAGE = "http://192.168.0.136:3000"
 // muhib 
-export const base_URL = "http://192.168.0.51:3000/api/v1"
-export const base_URL_IMAGE = "http://192.168.0.51:3000"
-// export const base_URL ="https://staging-askip.fr/api/v1"
-// export const base_URL_IMAGE="https://staging-askip.fr"
+// export const base_URL = "http://192.168.0.51:3000/api/v1"
+// export const base_URL_IMAGE = "http://192.168.0.51:3000"
+export const base_URL ="https://staging-askip.fr/api/v1"
+export const base_URL_IMAGE="https://staging-askip.fr"
